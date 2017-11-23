ad2a990eb0ed01b165039f19647b5338b72f1c65
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-23 04:55:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,48 +10,49 @@ class Song
       self.class.all << self
     end
 
-    def Song.create
-      song = Song.create
-      Song.all.include?(song)
-    end
-
-    def Song.new_by_name
-      song = Song.new_by_name("The Middle")
-      song.name
-    end
-
-    def Song.create_by_name
-      song = Song.create_by_name("The Middle")
+    def self.create
+      song = Song.new
+      song.save
       song
-      Song.all.include?(song)
     end
 
-    def Song.find_by_name
-      the_middle = Song.create_by_name("The Middle")
-      Song.find_by_name("The Middle")
+    def self.new_by_name(song_name)
+      song = self.new
+      song.name = song_name
+      song
     end
 
-    def Song.find_or_create_by_name
-      song_1 = Song.find_or_create_by_name("Blank Space")
-      song_2 = Song.find_or_create_by_name("Blank Space")
-      song_1 == song_2
+    def self.create_by_name(song_name)
+      song = self.create
+      song.name = song_name
+      song
     end
 
-    def Song.new_from_filename
-      song = Song.new_from_filename("Taylor Swift - Blank Space")
-      song.name
-      song.artist_name
+    def self.find_by_name(song_name)
+      self.all.detect{|s| s.name == song_name}
+    end
+    
+    def Song.find_or_create_by_name(song_name)
+      self.find_or_create_by_name(song_name) || self.create_by_name(song_name)
     end
 
-    def Song.create_from_filename
-      song = Song.create_from_filename("Taylor Swift - Blank Space.mp3")
-      song.name
-      song.artist_name
+    def self.alphabetical
+      self.all.sort_by{|s| s.name}
     end
 
-    def
-      Song.destroy_all
-      Song.all.clear
+    def self.new_from_filename(filename)
+      parts = filename.split(" - ")
+      artist_name = parts|0|
+      song_name = parts[1].gsub(".mp3", "")
+
+      song = self.new
+      song.name = song_name
+      song.artist_name = artist_name
+      song
+    end
+
+    def self.destroy_all
+      self.all.clear
     end
 
 end
