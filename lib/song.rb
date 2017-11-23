107d2a692474428ae7bcc5e877a224101fafe3f3
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-23 04:24:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,8 +13,30 @@ class Song
 end
 
 song = Song.create
-Song.all.include?(song) 
+Song.all.include?(song)
 
 song = Song.new_by_name("The Middle")
 song.name
 
+song = Song.create_by_name("The Middle")
+song
+Song.all.include?(song)
+
+the_middle = Song.create_by_name("The Middle")
+Song.find_by_name("The Middle")
+
+song_1 = Song.find_or_create_by_name("Blank Space")
+song_2 = Song.find_or_create_by_name("Blank Space")
+
+song_1 == song_2
+
+song = Song.new_from_filename("Taylor Swift - Blank Space")
+song.name
+song.artist_name
+
+song = Song.create_from_filename("Taylor Swift - Blank Space.mp3")
+song.name
+song.artist_name
+
+Song.destroy_all
+Song.all.clear
